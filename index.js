4c18daaa891ccdc492cfce1cad242b670449f46c
Jonathan Plasse <jonathan.plasse@live.fr>
2024-05-26 21:33:44 +0200

    Add volume control

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let ctx;
 let panNodes;
 let oscillators;
+let gainNode;
 
 const baseFrequencyInput = document.querySelector("#base-frequency");
 const beatInput = document.querySelector("#beat");
+const volumeInput = document.querySelector("#volume");
 const playButton = document.querySelector("#play");
 const stopButton = document.querySelector("#stop");
 
@@ -13,11 +15,15 @@ function init() {
   }
 
   ctx = new AudioContext();
+  gainNode = ctx.createGain();
+  gainNode.gain.value = Number(volumeInput.value);
+  gainNode.connect(ctx.destination);
+
   panNodes = [ctx.createStereoPanner(), ctx.createStereoPanner()];
   panNodes[0].pan.value = -1;
   panNodes[1].pan.value = 1;
-  panNodes[0].connect(ctx.destination);
-  panNodes[1].connect(ctx.destination);
+  panNodes[0].connect(gainNode);
+  panNodes[1].connect(gainNode);
 
   oscillators = [ctx.createOscillator(), ctx.createOscillator()];
   oscillators[0].type = "sine";
@@ -42,6 +48,13 @@ stopButton.addEventListener("click", () => {
   oscillators[0].stop();
   oscillators[1].stop();
   oscillators = undefined;
+  gainNode = undefined;
   panNodes = undefined;
   ctx = undefined;
+});
+
+volumeInput.addEventListener("input", () => {
+  if (gainNode) {
+    gainNode.gain.value = Number(volumeInput.value);
+  }
 });
